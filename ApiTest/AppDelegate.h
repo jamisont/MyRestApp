0a79ecf6766bd6d3cd87d59e418c3c16615e7c09
ios <ios@Macintosh-2.local>
2015-10-20 16:45:48 -0700

    first challenge commit

---
+++ ApiTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ApiTest
+//
+//  Created by ios on 10/20/15.
+//  Copyright © 2015 Brandon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ApiTest/ViewController.h
@@ -0,0 +1,18 @@
+//
+//  ViewController.h
+//  ApiTest
+//
+//  Created by ios on 10/20/15.
+//  Copyright © 2015 Brandon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@property (weak, nonatomic) IBOutlet UITextField *textFieldUsername;
+@property (weak, nonatomic) IBOutlet UITextField *textFieldPassword;
+@property (weak, nonatomic) IBOutlet UILabel *labelAuthToken;
+
+@end
+
