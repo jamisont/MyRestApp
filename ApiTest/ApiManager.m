8fea2364cc14472d032f1cbc3a0d415a19401c0b
Tasha Jamison <jamisont@vt.edu>
2015-10-28 12:02:33 -0700

    Defined implementation for registerNewUsername method in ApiManager

--- ApiTest/ApiManager.m
+++ ApiTest/ApiManager.m
@@ -58,7 +58,48 @@ NSString *SERVER_API_BASE_URL = @"http://localhost:5000";
 
 #pragma mark CHALLENGE #1 - let's do this together with a projector
 - (void)registerNewUsername:(NSString *)username withPassword:(NSString *)password completion:(void (^)(NSString *))completion failure:(void (^)(void))failure {
+        
+    NSURLSession *urlSession = [NSURLSession sharedSession];
+    NSURL *url = [NSURL URLWithString:@"http://104.236.231.254:5000/user"];
     
+    NSMutableURLRequest *request = [NSMutableURLRequest requestWithURL:url];
+    request.HTTPMethod = @"POST";
+    
+    NSMutableDictionary *userDataDictionary = [[NSMutableDictionary alloc] init];
+    [userDataDictionary setObject:username forKey:@"username"];
+    [userDataDictionary setObject:password forKey:@"password"];
+    NSError *error;
+    NSData *dataToPass = [NSJSONSerialization dataWithJSONObject:userDataDictionary options:0 error:&error];
+    request.HTTPBody = dataToPass;
+    
+    [request addValue:@"application/json" forHTTPHeaderField:@"Content-Type"];
+    
+    NSURLSessionDataTask *dataTask = [urlSession dataTaskWithRequest:request completionHandler:^(NSData * _Nullable data, NSURLResponse * _Nullable response, NSError * _Nullable error)
+    {
+        if (error)
+        {
+            NSLog(@"There was an error with response %ld", (long)((NSHTTPURLResponse *)response).statusCode);
+            failure();
+        }
+        else
+        {
+            NSLog(@"Success with response %ld", (long)((NSHTTPURLResponse *)response).statusCode);
+            if (((NSHTTPURLResponse *)response).statusCode == 200)
+            {
+                NSString *authToken = [[NSString alloc] initWithData:data encoding:NSUTF8StringEncoding];
+                completion(authToken);
+            }
+            else
+            {
+                failure();
+            }
+        }
+    }];
+    
+    [dataTask resume];
+
+    
+    // what needs to happen next?
 }
 
 #pragma mark CHALLENGE #2 - with a partner
